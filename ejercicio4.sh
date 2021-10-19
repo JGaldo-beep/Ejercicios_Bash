9f407ac63d09dc5004a03d84f23b05888e09bbee
JGaldo-beep <galdiros@gmail.com>
2021-10-19 09:21:39 -0400

    Modificado

--- ejercicio4.sh
+++ ejercicio4.sh
@@ -4,7 +4,7 @@ read -p "Ingrese el nombre del fichero: " fichero
 if [[ -e $fichero ]]; then
   propietario=`ls -l $fichero | awk '{print $2}'`
   echo "El propietario del fichero $fichero es $propietario"
-  echo "El fichero $fichero ocupa "
+  echo "El fichero $fichero ocupa " #Falta conocer el tamaño del fichero
 else
   echo "El fichero $fichero no existe"
 fi
