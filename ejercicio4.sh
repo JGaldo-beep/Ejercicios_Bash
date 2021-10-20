543845a9b907df2050c73a0a3208d4d13ae9f36f
Galdo <80436377+JGaldo-beep@users.noreply.github.com>
2021-10-19 21:49:48 -0400

    Update ejercicio4.sh

--- ejercicio4.sh
+++ ejercicio4.sh
@@ -2,9 +2,10 @@
 
 read -p "Ingrese el nombre del fichero: " fichero
 if [[ -e $fichero ]]; then
-  propietario=`ls -l $fichero | awk '{print $2}'`
+  propietario=`ls -l $fichero | awk '{print $3}'`
+  peso=`ls -l $fichero | awk '{print $5}'`
   echo "El propietario del fichero $fichero es $propietario"
-  echo "El fichero $fichero ocupa " #Falta conocer el tamaño del fichero
+  echo "El fichero '$fichero' ocupa $peso bytes " #Falta conocer el tamaño del fichero
 else
   echo "El fichero $fichero no existe"
 fi
